892fc49d9b490df32fcf5fe26ca76b94aa423bd0
Alex <alex_stewart1@hotmail.com>
2017-08-14 17:46:33 +1200

    First commit, nothing yet.

---
+++ app/src/main/java/com/example/alex/raiseyourgameapp/DBController.java
@@ -0,0 +1,13 @@
+package com.example.alex.raiseyourgameapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DBController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dbcontroller);
+    }
+}
