2374997ae3b9c24b57a332789e6469a55b00cd01
fgmind <fgmind@outlook.com>
2017-09-03 16:57:16 +1200

    Added timer to title screen, now working as a splash screen

--- app/src/main/java/com/example/alex/raiseyourgameapp/TitleScreen.java
+++ app/src/main/java/com/example/alex/raiseyourgameapp/TitleScreen.java
@@ -2,6 +2,7 @@ package com.example.alex.raiseyourgameapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,8 @@ import static com.example.alex.raiseyourgameapp.R.layout.activity_title_screen;
 public class TitleScreen extends AppCompatActivity implements View.OnClickListener {
     DBController db;
     MainController mc;
+    private static int SPLASH_TIMEOUT = 4000; // screen timeout before loading getSkills
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,15 @@ public class TitleScreen extends AppCompatActivity implements View.OnClickListen
 
         Button button = (Button) findViewById(R.id.changeScreen);
         button.setOnClickListener(this);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent levelIntent = new Intent(TitleScreen.this, LevelActivity.class);
+                startActivity(levelIntent);
+                finish();
+            }
+        },SPLASH_TIMEOUT);
 
     }
     public void onClick(View v) {
